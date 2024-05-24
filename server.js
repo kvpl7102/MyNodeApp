fa14f771f8168e69ea91ea085fe454a71190e639
kinhvi_pl <kinhvi0710@gmail.com>
2024-05-24 00:03:08 -0400

    commit 24/5: setting up database collections

--- server.js
+++ server.js
@@ -1,28 +1,52 @@
-const express = require('express');
-const moogoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
 const uri =
-  "mongodb+srv://kinhvi0710:admin123@mynodeapp.vfzn57e.mongodb.net/?retryWrites=true&w=majority&appName=MyNodeApp";
+  "mongodb+srv://kinhvi0710:admin123@mynodeapp.vfzn57e.mongodb.net/MyNodeAppDB?retryWrites=true&w=majority&appName=MyNodeApp";
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+
+// app.get("/", (req, res) => {
+//   res.send("What's up!!!");
+// });
+
+// Endpoints to get city and zip code from a given GPS location
+app.get("/location", (req, res) => {
+  
+  
 });
 
-async function connect() {
-    try {
-        await moogoose.connect(uri);
-        console.log('Connected succesfully to MongoDB');
-    }
-    catch (err) {
-        console.log('Error connecting to the database' + err);
-    }
-}
 
-connect();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+
+
+
+
+
+
+
+
+
+
+
+
+
+mongoose.set("strictQuery", false);
+mongoose.connect(uri).then(() => {
+  console.log("Connected to the database!");
+  
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+
+}).catch((err) => {
+  console.log("Cannot connect to the database!", err);
+  process.exit();
 });
 
+// connect();
 
