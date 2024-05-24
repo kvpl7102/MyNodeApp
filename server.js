0745543363657b8bca8fcd49c66aedb677ba0f9c
kinhvi_pl <kinhvi0710@gmail.com>
2024-05-24 01:05:37 -0400

    commite 24/5: create API to query city and zipcode from a GPS location

--- server.js
+++ server.js
@@ -7,32 +7,31 @@ const uri =
   "mongodb+srv://kinhvi0710:admin123@mynodeapp.vfzn57e.mongodb.net/MyNodeAppDB?retryWrites=true&w=majority&appName=MyNodeApp";
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
 
-
-// app.get("/", (req, res) => {
-//   res.send("What's up!!!");
-// });
-
-// Endpoints to get city and zip code from a given GPS location
-app.get("/location", (req, res) => {
-  
-  
+app.get("/", (req, res) => {
+  res.send("What's up!!!");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Endpoints to get city and zip code from a given GPS location
+app.get("/location", async (req, res) => {
+  try {
+    const { lat, long } = req.query;
+    const docs = await Zipcode.find({
+      "center.latitude": lat,
+      "center.longitude": long,
+    });
+    const result = docs.map((doc) => {
+      return {
+        zipcode: doc.zipcode,
+        city: doc.city,
+      };
+    });
+    res.send(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 
 
